2578c94a9f595bc4ff65f5d3302edb495d40e51b
goodness5 <goodnesskolapo@gmail.com>
2025-02-17 23:46:32 +0100

    testing avnu handler

--- src/app/crimerecorder/components/Recording.jsx
+++ src/app/crimerecorder/components/Recording.jsx
@@ -164,7 +164,7 @@ export const Recording = ({ text, icon1, imgText, category }) => {
 
             
             const signature = await starknetJsAccount.signer.signMessage(
-              typedData,    
+              typedData.typedData,    
               account.address
             );
             console.log("signature...",signature)
